60ebf3fbad9d5708e52a2ae5d703f0b6780ac94e
CrisRod <crodriguez85@gmail.com>
2019-05-20 15:51:28 -0400

    Index models

--- server/models/index.js
+++ server/models/index.js
@@ -1,38 +1,18 @@
-let users = [{
-    id: 1,
-    name: 'Tamas',
-    cars: [1, 2]
-  }, {
-    id: 2,
-    name: 'Susan',
-    cars: []
-  }, {
-    id: 3,
-    name: 'Steven',
-    cars: [3]
-  }];
-  
-  let cars = [{
-    id: 1,
-    make: 'Ford',
-    model: 'Focus',
-    colour: 'red',
-    ownedBy: 1
-  }, {
-    id: 2,
-    make: 'Fiat',
-    model: '500',
-    colour: 'blue',
-    ownedBy: 1
-  }, {
-    id: 3,
-    make: 'Mercedes',
-    model: 'C250',
-    colour: 'silver',
-    ownedBy: 3
-  }];
-  
-  module.exports = {
-    users,
-    cars
-  };
+const { sequelize } = require('./database');
+
+const UserModel = sequelize.import('./user');
+const CarModel = sequelize.import('./car');
+
+const models = {
+  User: UserModel,
+  Car: CarModel
+};
+
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+module.exports = models;
+
