b6bb8ec84af8e88822ee4b8f0fa46384d132c425
CrisRod <crodriguez85@gmail.com>
2019-05-20 17:22:03 -0400

    Added to app ApolloClient and ApolloProvider

--- client/fleet-app/src/App.js
+++ client/fleet-app/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-function App() {
-  return null;
+const client = new ApolloClient({
+  uri: 'http://localhots:4000/graphql'
+});
+
+class App extends Component () {
+  render() {
+    return (
+      <div>
+        <ApolloProvider client={client}>
+        </ApolloProvider>
+      </div>
+    )
+  }
 }
 
 export default App;
