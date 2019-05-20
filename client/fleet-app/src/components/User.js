0161e4e503db0d4252efb9855f7305eb94931e95
CrisRod <crodriguez85@gmail.com>
2019-05-20 18:21:22 -0400

    Mutation AddUser with component

--- client/fleet-app/src/components/User.js
+++ client/fleet-app/src/components/User.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import query from '../graphql/query';
+import AddUser from './AddUser';
 
 export default class User extends Component {
   render() {
     return (
       <div>
+      <AddUser />
+      <hr/>
         <Query query={query}>
           {({ data, loading }) => {
             if (loading) return <p>Loading...</p>;
