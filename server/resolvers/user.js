d9f2b5f599a1ec6f55f60ec6b516eaee8e4bee50
CrisRod <crodriguez85@gmail.com>
2019-05-22 14:34:51 -0400

    Cloudinary config on user resolver

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,4 +1,11 @@
 const jwt = require('jsonwebtoken');
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
 //Creating JWT
 const createToken = (user, secret, expiresIn) => {
