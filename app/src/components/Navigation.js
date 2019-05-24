b4476686f24af19d664540536cf21a89cfc0d773
CrisRod <crodriguez85@gmail.com>
2019-05-24 15:03:59 -0400

    Starting wih Navigation Component

---
+++ app/src/components/Navigation.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NavigationNoAuth = () => (
+    <ul>
+        <li>Login</li>
+        <li>Register</li>
+    </ul>
+);
+
+// const NavigationWithAUth = () => (
+
+// );
+
+
+const Navigation = () => (
+    <div>
+        <NavigationNoAuth/>
+    </div>
+
+);
+
+export default Navigation;
