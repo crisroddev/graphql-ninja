7852560e395a235886f0759d658ea090803225df
CrisRod <crodriguez85@gmail.com>
2019-05-24 12:08:08 -0400

    Multer for uploading image

--- server/app.js
+++ server/app.js
@@ -11,6 +11,18 @@ const resolvers = require('./resolvers');
 
 const jwt = require('jsonwebtoken');
 
+// Multer
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function( req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+const upload = multer({ storage });
+
 // Routes
 const routes = require('./routes');
 
@@ -43,6 +55,7 @@ app.set('views', `${__dirname}/public`);
 
 app.get('/', routes.index);
 app.get('/user/:id', routes.userInfo);
+app.post('/upload', upload.fields([{ name: 'file'}, { name: 'id' }]), routes.upload)
 
 app.use(cors());
 
