ee92991bddcdd3dcba11452878aef8e1d0605525
CrisRod <crodriguez85@gmail.com>
2019-05-24 15:33:59 -0400

    Register Form and mutation

--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default class RegisterPage extends Component {
+const registerUser = gql`
+    mutation register($name: String!, $username: Strin!, $password: String!){
+        register(name: $name, username: $username, passwrod: $password)
+    }
+`;
+
+const RegisterPage = () => (
+    <div>
+        <h2>Register</h2>
+        <RegisterForm/>
+    </div>
+)
+
+class RegisterForm extends Component {
+    state= {
+        name: '',
+        username: '',
+        passwrod: ''
+    };
+
     render() {
-        return (
-            <div>
-                <h2>Register</h2>
-            </div>
+        return(
+            <p>Hola</p>
         )
     }
-};
+}
+
+
+export default RegisterPage;
