765d00001e95f4563a8329986192c06e498d4058
CrisRod <crodriguez85@gmail.com>
2019-05-24 16:16:23 -0400

    Fixing index app react

--- app/src/index.js
+++ app/src/index.js
@@ -6,18 +6,18 @@ import * as serviceWorker from './serviceWorker';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
-const client = new ApolloClient({
-    uri: 'http:localhost:4000/graphql'
+export const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+
 });
 
-
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>
-    , document.getElementById('root'));
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
