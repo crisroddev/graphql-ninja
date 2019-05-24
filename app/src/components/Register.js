2c6042868cee95660de779cb5eba440d3e463f5d
CrisRod <crodriguez85@gmail.com>
2019-05-24 15:59:35 -0400

    REGISTER FORM FINISHED

--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -3,112 +3,103 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const registerUser = gql`
-    mutation register($name: String!, $username: Strin!, $password: String!){
-        register(name: $name, username: $username, passwrod: $password)
-    }
+mutation register($name: String!, $username: String!, $password: String!) {
+  register(name: $name, username: $username, password: $password)
+}
 `;
 
 const RegisterPage = () => (
-    <div>
-        <h2>Register</h2>
-        <RegisterForm/>
-    </div>
-)
+  <div>
+    <h2>Register</h2>
+    <RegisterForm />
+  </div>
+);
 
 class RegisterForm extends Component {
-    state= {
-        name: '',
-        username: '',
-        passwrod: ''
-    };
+  state = {
+    name: '',
+    username: '',
+    password: '',
+    success: false
+  };
 
-    nameChanged = ({ target: { value }}) => {
-        this.setState({
-            name: value
-        });
-    }
+  nameChanged = ({ target: { value }}) => {
+    this.setState({ name: value });
+  }
+  usernameChanged = ({ target: { value }}) => {
+    this.setState({ username: value });
+  }
+  passwordChanged = ({ target: { value }}) => {
+    this.setState({ password: value });
+  }
+  resetFields = () => {
+    this.setState({ name: '' });
+    this.setState({ username: '' });
+    this.setState({ password: '' });
+    this.setState({ success: false });
+  }
 
-    usernameChanged = ({ target: { value }}) => {
-        this.setState({
-            username: value
-        });
-    }
+  render() {
+    const validForm = 
+      this.state.name !== '' &&
+      this.state.username !== '' &&
+      this.state.password !== '';
 
-    passwordChanged = ({ target: { value }}) => {
-        this.setState({
-            password: value
-        });
-    }
-
-    resetFields = () => {
-        this.setState({ name: ''});
-        this.setState({ username: ''});
-        this.setState({ password: ''});
-    }
-
-
-    render() {
-        const validForm =
-            this.state.name !== '' &&
-            this.state.name !== '' &&
-            this.state.passwrod !== '';
-            
-        return(
-            <Mutation mutation={registerUser}>
-            {( register, { loading, error }) => (
-              <form
-                onSubmit={evt => {
-                  evt.preventDefault();
-                  register({
-                    variables: {
-                      name: this.state.name,
-                      username: this.state.username,
-                      password: this.state.password
-                    }
-                  }).then(({ data }) => {
-                    this.setState({
-                      success: data.register
-                    });
-                  }).catch(error => console.error(error));
-                  this.resetFields();
-                }}
-                >
-                <label>
-                  <span>Name</span>
-                  <input
-                    type="text"
-                    value={this.state.name}
-                    onChange={this.nameChanged}
-                  />
-                </label>
-                <label>
-                  <span>Username</span>
-                  <input
-                    type="text"
-                    value={this.state.username}
-                    onChange={this.usernameChanged}
-                  />
-                </label>
-                <label>
-                  <span>Password</span>
-                  <input
-                    type="password"
-                    value={this.state.password}
-                    onChange={this.passwordChanged}
-                  />
-                </label>
-                <div>
-                  <button disabled={!validForm}>Register</button>
-                </div>
-                { loading && <p>Adding user...</p> }
-                { error && <p>Error, did not register user.</p> }
-                { this.state.success && <p>Registration successful.</p> }
-                </form>
-            )}
-            </Mutation>
-        )
-    }
+    return (
+      <Mutation mutation={registerUser}>
+      {( register, { loading, error }) => (
+        <form
+          onSubmit={evt => {
+            evt.preventDefault();
+            register({
+              variables: {
+                name: this.state.name,
+                username: this.state.username,
+                password: this.state.password
+              }
+            }).then(({ data }) => {
+              this.setState({
+                success: data.register
+              });
+            }).catch(error => console.error(error));
+            this.resetFields();
+          }}
+          >
+          <label>
+            <span>Name</span>
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.nameChanged}
+            />
+          </label>
+          <label>
+            <span>Username</span>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.usernameChanged}
+            />
+          </label>
+          <label>
+            <span>Password</span>
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.passwordChanged}
+            />
+          </label>
+          <div>
+            <button disabled={!validForm}>Register</button>
+          </div>
+          { loading && <p>Adding user...</p> }
+          { error && <p>Error, did not register user.</p> }
+          { this.state.success && <p>Registration successful.</p> }
+          </form>
+      )}
+      </Mutation>
+    );
+  }
 }
-
 
 export default RegisterPage;
