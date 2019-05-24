b069bcaf7738e69c9282cbd1c05818e7d8017d0e
CrisRod <crodriguez85@gmail.com>
2019-05-24 16:20:35 -0400

    Login gql mutation

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,4 +1,15 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const loginUser = gql`
+    mutation login($username: String!, $password: String!){
+        login(username: $username, password: $password){
+            token
+        }
+    }
+`;
+
 
 export default class LoginPage extends Component {
     render() {
