a31c9f66bdf49fce05b9b0f34b91a66bf52c96aa
CrisRod <crodriguez85@gmail.com>
2019-05-24 15:52:24 -0400

    Functions on change

--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -22,9 +22,79 @@ class RegisterForm extends Component {
         passwrod: ''
     };
 
+    nameChanged = ({ target: { value }}) => {
+        this.setState({
+            name: value
+        });
+    }
+
+    usernameChanged = ({ target: { value }}) => {
+        this.setState({
+            username: value
+        });
+    }
+
+    passwordChanged = ({ target: { value }}) => {
+        this.setState({
+            password: value
+        });
+    }
+
+
     render() {
         return(
-            <p>Hola</p>
+            <Mutation mutation={registerUser}>
+            {( register, { loading, error }) => (
+              <form
+                onSubmit={evt => {
+                  evt.preventDefault();
+                  register({
+                    variables: {
+                      name: this.state.name,
+                      username: this.state.username,
+                      password: this.state.password
+                    }
+                  }).then(({ data }) => {
+                    this.setState({
+                      success: data.register
+                    });
+                  }).catch(error => console.error(error));
+                  this.resetFields();
+                }}
+                >
+                <label>
+                  <span>Name</span>
+                  <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.nameChanged}
+                  />
+                </label>
+                <label>
+                  <span>Username</span>
+                  <input
+                    type="text"
+                    value={this.state.username}
+                    onChange={this.usernameChanged}
+                  />
+                </label>
+                <label>
+                  <span>Password</span>
+                  <input
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.passwordChanged}
+                  />
+                </label>
+                <div>
+                  <button disabled={!validForm}>Register</button>
+                </div>
+                { loading && <p>Adding user...</p> }
+                { error && <p>Error, did not register user.</p> }
+                { this.state.success && <p>Registration successful.</p> }
+                </form>
+            )}
+            </Mutation>
         )
     }
 }
