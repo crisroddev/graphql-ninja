d65733ed86f688ecdec53cf05a98af649a22a376
CrisRod <crodriguez85@gmail.com>
2019-05-24 11:06:28 -0400

    First query graphql

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,31 @@
-const index = (req, res) => {
-    const query = ``;
-    return res.render('index');
+const fetch = require('node-fetch');
+
+function fetchGraphQL(query, variables = {}) {
+    const response = await ('http://localhost:3000/graphql', {
+        method: 'POST',
+        header: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            query,
+            variables
+        })
+    });
+    return response.json();
+
+}
+
+const index = async (req, res) => {
+    const query = `{
+        users {
+            id
+            name
+        }
+    }`;
+    const response = await fetchGraphQL(query);
+    return res.render('index' ,{
+        users: response.data.users
+    });
 };
 
 module.exports = {
