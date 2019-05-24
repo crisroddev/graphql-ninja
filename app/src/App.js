e2016753155172c23b86a5a99b7f88a39c07a31f
CrisRod <crodriguez85@gmail.com>
2019-05-24 15:08:10 -0400

    First Routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Router>
+          <Fragment>
+            <Navigation/>
+            <hr/>
+            <Route path="/login" component={}/>
+            <Route path="/register" component={}/>
+          </Fragment>
+        </Router>
       </div>
     )
   }
